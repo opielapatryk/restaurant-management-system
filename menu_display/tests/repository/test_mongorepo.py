98cd0172186ade21f8d0b426a462e86c9ab2989b
opielapatryk <xpatrykopiela@gmail.com>
2024-05-16 20:58:21 +0200

    test version of k8s deployment

--- menu_display/tests/repository/test_mongorepo.py
+++
@@ -1,8 +0,0 @@
-from menu_display.repositories.mongorepo import MongoRepo
-
-def test_repository_list_without_parameters(mg_database, mg_test_data):
-    repo = MongoRepo()
-
-    repo_menu = repo.list()
-
-    assert repo_menu['name'] == mg_test_data['name']
