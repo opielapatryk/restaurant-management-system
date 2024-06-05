8cda4cb204654564b24ca1aa2c79570b3c928996
opielapatryk <xpatrykopiela@gmail.com>
2024-06-05 13:12:47 +0200

    refactor

--- menu_config/main.py
+++ menu_config/main.py
@@ -13,10 +13,13 @@ from domain.auth.auth import AuthData
 from domain.auth.token import TokenData
 
 # Third party modules
-from fastapi import FastAPI,status,HTTPException,Depends
+from fastapi import FastAPI, status, HTTPException, Depends
 
-app = FastAPI(docs_url="/api/v1/config/docs",openapi_url="/api/v1/config/openapi.json")
+app = FastAPI(
+    docs_url="/api/v1/config/docs", openapi_url="/api/v1/config/openapi.json"
+)
 auth_client = AuthClient()
+
 
 # Auth
 @app.post("/login")
@@ -24,7 +27,11 @@ def login(auth_data: AuthData):
     response = auth_client.authenticate(auth_data.email, auth_data.password)
     if not response.success:
         raise HTTPException(status_code=401, detail=response.message)
-    return {"access_token": response.access_token, "refresh_token": response.refresh_token}
+    return {
+        "access_token": response.access_token,
+        "refresh_token": response.refresh_token,
+    }
+
 
 # To use on the client side by catching 401 error
 @app.post("/refresh-token")
@@ -32,7 +39,11 @@ def refresh_token(token_data: TokenData):
     response = auth_client.refresh_token(token_data.token)
     if not response.success:
         raise HTTPException(status_code=401, detail=response.message)
-    return {"access_token": response.access_token, "refresh_token": response.refresh_token}
+    return {
+        "access_token": response.access_token,
+        "refresh_token": response.refresh_token,
+    }
+
 
 def verify_token(token_data: TokenData):
     response = auth_client.verify_token(token_data.token)
@@ -40,19 +51,27 @@ def verify_token(token_data: TokenData):
         raise HTTPException(status_code=401, detail=response.message)
     return {"message": response.message}
 
+
 # RabbitMQ
-def produce_message(message,service: ProducerService = ProducerService, broker: RabbitMQProducer = RabbitMQProducer):
+def produce_message(
+    message,
+    service: ProducerService = ProducerService,
+    broker: RabbitMQProducer = RabbitMQProducer,
+):
     try:
         service(broker).send_message(message)
         return {"status": "Message sent"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 class EntityDoesNotExist(Exception):
     """Raised when entity was not found in database."""
 
+
 # API
-@app.get("/api/v1/config",
+@app.get(
+    "/api/v1/config",
     status_code=status.HTTP_200_OK,
     name="get_menu",
 )
@@ -67,86 +86,100 @@ def get_menu(token: TokenData = Depends(verify_token)):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Menu not found!"
         )
-    
-@app.get("/api/v1/config/{id}",
+
+
+@app.get(
+    "/api/v1/config/{id}",
     status_code=status.HTTP_200_OK,
     name="get_menu",
 )
-def get_menu_by_id(id,token: TokenData = Depends(verify_token)):
+def get_menu_by_id(id, token: TokenData = Depends(verify_token)):
     try:
         repo = MongoRepo()
         menu = menu_get_use_case(repo, id)
-        
+
         return menu
 
     except EntityDoesNotExist:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Menu not found!"
         )
-    
-@app.post("/api/v1/config/",
+
+
+@app.post(
+    "/api/v1/config/",
     status_code=status.HTTP_201_CREATED,
     name="post_menu",
 )
-def post_menu(menu: dict,token: TokenData = Depends(verify_token)):
+def post_menu(menu: dict, token: TokenData = Depends(verify_token)):
     repo = MongoRepo()
 
     try:
-      result = menu_post_use_case(repo, menu)
-      
-      if menu.get('active') == True:
-        produce_message(result)
-      
-      return result
+        result = menu_post_use_case(repo, menu)
+
+        if menu.get("active") == True:
+            produce_message(result)
+
+        return result
     except:
-      raise HTTPException(status_code=400, detail="Menu already exists or missing required fields")
-    
-@app.put("/api/v1/config/{id}",
+        raise HTTPException(
+            status_code=400,
+            detail="Menu already exists or missing required fields",
+        )
+
+
+@app.put(
+    "/api/v1/config/{id}",
     status_code=status.HTTP_201_CREATED,
     name="put_menu",
 )
-def put_menu(menu: dict,id,token: TokenData = Depends(verify_token)):
+def put_menu(menu: dict, id, token: TokenData = Depends(verify_token)):
     repo = MongoRepo()
 
     try:
-      result = menu_put_use_case(repo, menu, id)
+        result = menu_put_use_case(repo, menu, id)
 
-      if menu.get('active') == True:
-        produce_message(result)
+        if menu.get("active") == True:
+            produce_message(result)
 
-      return result
+        return result
     except:
-      raise HTTPException(status_code=400, detail="Failed to update menu")
-    
-@app.patch("/api/v1/config/{id}",
+        raise HTTPException(status_code=400, detail="Failed to update menu")
+
+
+@app.patch(
+    "/api/v1/config/{id}",
     status_code=status.HTTP_200_OK,
     name="patch_menu",
 )
-def patch_menu(menu_fields: dict,id,token: TokenData = Depends(verify_token)):
+def patch_menu(
+    menu_fields: dict, id, token: TokenData = Depends(verify_token)
+):
     repo = MongoRepo()
 
     try:
-      result = menu_patch_use_case(repo, menu_fields, id)
+        result = menu_patch_use_case(repo, menu_fields, id)
 
-      menu_active = menu_get_use_case(repo, id)['active']
+        menu_active = menu_get_use_case(repo, id)["active"]
 
-      if menu_active == True:
-        produce_message(result)
-      
-      return result
+        if menu_active == True:
+            produce_message(result)
+
+        return result
     except:
-      raise HTTPException(status_code=400, detail="Failed to update menu")
-    
-@app.delete("/api/v1/config/{id}",
+        raise HTTPException(status_code=400, detail="Failed to update menu")
+
+
+@app.delete(
+    "/api/v1/config/{id}",
     status_code=status.HTTP_200_OK,
     name="delete_menu",
 )
-def delete_menu(id,token: TokenData = Depends(verify_token)):
+def delete_menu(id, token: TokenData = Depends(verify_token)):
     repo = MongoRepo()
 
     try:
-      result = menu_delete_use_case(repo, id)
-      return result
+        result = menu_delete_use_case(repo, id)
+        return result
     except:
-      raise HTTPException(status_code=400, detail="Failed to delete menu")
-    
+        raise HTTPException(status_code=400, detail="Failed to delete menu")
