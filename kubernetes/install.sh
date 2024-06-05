2e74e39b848075144208dffef1403d69a345d016
opielapatryk <xpatrykopiela@gmail.com>
2024-06-05 13:20:13 +0200

    edit script

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -1,30 +1,96 @@
-helm repo add hashicorp https://helm.releases.hashicorp.com && \
-helm install --values values-v1.yaml consul hashicorp/consul --version "1.2.0" && \
-kubectl wait --for=condition=available --timeout=90s deployment/consul-connect-injector && \
-kubectl apply -f proxy-defaults.yaml && \
-kubectl wait --for=condition=available --timeout=90s deployment/consul-connect-injector && \
-kubectl apply -f redis.yaml && \
-kubectl wait --for=condition=available --timeout=90s deployment/consul-connect-injector && \
-kubectl apply -f rabbitmq.yaml && \
-kubectl wait --for=condition=available --timeout=90s deployment/consul-connect-injector && \
-kubectl apply -f mongodb.yaml && \
-kubectl wait --for=condition=available --timeout=90s deployment/consul-connect-injector && \
-kubectl apply -f menu-config-mongodb.yaml && \
-kubectl wait --for=condition=available --timeout=90s deployment/consul-connect-injector && \
-kubectl apply -f menu.yaml && \
-kubectl wait --for=condition=available --timeout=90s deployment/consul-connect-injector && \
-kubectl apply -f menu-config.yaml && \
-kubectl wait --for=condition=available --timeout=90s deployment/consul-connect-injector && \
-kubectl apply -f postgres.yaml && \
-kubectl wait --for=condition=available --timeout=90s deployment/consul-connect-injector && \
-kubectl apply -f auth.yaml && \
-kubectl wait --for=condition=available --timeout=90s deployment/consul-connect-injector && \
-kubectl apply -f nginx.yaml && \
-kubectl apply -f allow.yaml && \
-kubectl apply -f consul-api-gateway.yaml && \
-kubectl apply -f routes.yaml && \
-kubectl apply -f referencegrant.yaml && \
-kubectl apply -f rbac.yaml && \
-echo "#######################################" && \
-echo "Deployment Complete" && \
+#!/bin/bash
+
+set -e
+
+function retry_until_success {
+  local cmd=$1
+  local yaml_file=$2
+
+  until $cmd; do
+    echo "Retrying $yaml_file due to previous failure..."
+    sleep 5
+  done
+}
+
+helm repo add hashicorp https://helm.releases.hashicorp.com
+
+helm repo update
+
+helm install --values values-v1.yaml consul hashicorp/consul --version '1.2.0'
+
+retry_until_success "kubectl apply -f proxy-defaults.yaml" "proxy-defaults.yaml"
+
+retry_until_success "kubectl apply -f redis.yaml" "redis.yaml"
+
+retry_until_success "kubectl apply -f rabbitmq.yaml" "rabbitmq.yaml"
+
+retry_until_success "kubectl apply -f mongodb.yaml" "mongodb.yaml"
+
+retry_until_success "kubectl apply -f menu-config-mongodb.yaml" "menu-config-mongodb.yaml"
+
+retry_until_success "kubectl apply -f menu.yaml" "menu.yaml"
+
+retry_until_success "kubectl apply -f menu-config.yaml" "menu-config.yaml"
+
+retry_until_success "kubectl apply -f postgres.yaml" "postgres.yaml"
+
+retry_until_success "kubectl apply -f auth.yaml" "auth.yaml"
+
+retry_until_success "kubectl apply -f nginx.yaml" "nginx.yaml"
+
+retry_until_success "kubectl apply -f allow.yaml" "allow.yaml"
+
+retry_until_success "kubectl apply -f consul-api-gateway.yaml" "consul-api-gateway.yaml"
+
+retry_until_success "kubectl apply -f routes.yaml" "routes.yaml"
+
+retry_until_success "kubectl apply -f referencegrant.yaml" "referencegrant.yaml"
+
+retry_until_success "kubectl apply -f rbac.yaml" "rbac.yaml"
+
 echo "#######################################"
+echo "Deployment Complete"
+echo "Grab your coffe and come back in 15 minutes :)"
+echo "Endpoints:"
+echo "Authentication: http://127.0.0.1:8000/api/v1/schema/auth"
+echo "Cart: http://127.0.0.1:8000/api/v1/schema/cart"
+echo "Config: http://127.0.0.1:8000/api/v1/schema/config"
+echo "Display: http://127.0.0.1:8000/api/v1/schema/display"
+echo "Order: http://127.0.0.1:8000/api/v1/schema/order"
+echo "#######################################"
+
+sleep 900 # Wait for services installation
+
+# Determine the operating system and open URLs accordingly
+OS="$(uname)"
+if [ "$OS" == "Linux" ]; then
+    if command -v xdg-open > /dev/null; then
+        xdg-open "http://127.0.0.1:8000/api/v1/schema/auth"
+        xdg-open "http://127.0.0.1:8000/api/v1/schema/cart"
+        xdg-open "http://127.0.0.1:8000/api/v1/schema/config"
+        xdg-open "http://127.0.0.1:8000/api/v1/schema/display"
+        xdg-open "http://127.0.0.1:8000/api/v1/schema/order"
+    elif command -v gnome-open > /dev/null; then
+        gnome-open "http://127.0.0.1:8000/api/v1/schema/auth"
+        gnome-open "http://127.0.0.1:8000/api/v1/schema/cart"
+        gnome-open "http://127.0.0.1:8000/api/v1/schema/config"
+        gnome-open "http://127.0.0.1:8000/api/v1/schema/display"
+        gnome-open "http://127.0.0.1:8000/api/v1/schema/order"
+    fi
+elif [ "$OS" == "Darwin" ]; then
+    open "http://127.0.0.1:8000/api/v1/schema/auth"
+    open "http://127.0.0.1:8000/api/v1/schema/cart"
+    open "http://127.0.0.1:8000/api/v1/schema/config"
+    open "http://127.0.0.1:8000/api/v1/schema/display"
+    open "http://127.0.0.1:8000/api/v1/schema/order"
+elif [ "$OS" == "CYGWIN" ] || [ "$OS" == "MINGW" ] || [ "$OS" == "MSYS" ]; then
+    start "http://127.0.0.1:8000/api/v1/schema/auth"
+    start "http://127.0.0.1:8000/api/v1/schema/cart"
+    start "http://127.0.0.1:8000/api/v1/schema/config"
+    start "http://127.0.0.1:8000/api/v1/schema/display"
+    start "http://127.0.0.1:8000/api/v1/schema/order"
+else
+    echo "Unsupported OS: $OS"
+fi
+
+kubectl get pods --watch
