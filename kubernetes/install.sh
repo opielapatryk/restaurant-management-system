046255e4ee950efeb61e11ddecf3b3882b110cf2
opielapatryk <xpatrykopiela@gmail.com>
2024-06-17 14:23:48 +0200

    deploy

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -16,7 +16,8 @@ helm repo add hashicorp https://helm.releases.hashicorp.com
 
 helm repo update
 
-helm install --values values.yaml consul hashicorp/consul
+# helm install --values values-eks.yaml consul hashicorp/consul #aws
+helm install --values values.yaml consul hashicorp/consul --version "1.2.0"
 
 retry_until_success "kubectl apply -f proxy-defaults.yaml" "proxy-defaults.yaml"
 
@@ -36,6 +37,16 @@ retry_until_success "kubectl apply -f postgres.yaml" "postgres.yaml"
 
 retry_until_success "kubectl apply -f auth.yaml" "auth.yaml"
 
+retry_until_success "kubectl apply -f order-mongodb.yaml" "order-mongodb.yaml"
+
+retry_until_success "kubectl apply -f order-grpc.yaml" "order-grpc.yaml"
+
+retry_until_success "kubectl apply -f order.yaml" "order.yaml"
+
+retry_until_success "kubectl apply -f cart-redis.yaml" "cart-redis.yaml"
+
+retry_until_success "kubectl apply -f cart.yaml" "cart.yaml"
+
 retry_until_success "kubectl apply -f nginx.yaml" "nginx.yaml"
 
 retry_until_success "kubectl apply -f allow.yaml" "allow.yaml"
@@ -52,6 +63,8 @@ echo "#######################################"
 echo "Deployment Complete"
 echo "Grab your coffe and come back in 15 minutes :)"
 echo "Endpoints:"
-echo "Config: http://API-GATEWAY:8000/api/v1/config/docs"
-echo "Display: http://API-GATEWAY:8000/api/v1/display/docs"
+echo "Config: http://127.0.0.1:8000/api/v1/config/docs"
+echo "Display: http://127.0.0.1:8000/api/v1/display/docs"
+echo "Cart: http://127.0.0.1:8000/api/v1/cart/docs"
+echo "Orders: http://127.0.0.1:8000/api/v1/orders/docs"
 echo "#######################################"
